8765c41d62ec6a02b443cdcaac41cb5e29aae6c0
jannatulferdous0391 <jannatul.psy.jnu@gmail.com>
2025-05-14 19:46:31 +0600

    Day 4 Practice

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/Day 4.R
@@ -0,0 +1,66 @@
+# Data Types in R
+# 1. Numeric - Integer/ Floating Point
+# String/ Character Type - "Jannatul" / 'Jannatul'
+# Logical
+
+a = 11
+b = 7.19 
+  
+class(a)
+class (b)
+
+name = "jannatul"
+
+print(name)
+
+class(name)
+
+
+
+
+# Logical Data Type - TRUE, FALSE (T, F)
+
+print(a)
+
+a > b
+
+class(a > b)
+class (a < b)
+
+print(a > b)
+print(a < b)
+
+x = 19
+y = 22-3
+
+print(x == y)
+print(x != y)
+print(y >= x)
+
+
+# Logical Operators in R
+4 == 4 && 5 ==5
+# && Logical operator
+# || Logical operator
+
+x != y || x == y
+
+# Simple Mathematical Operators in R
+
+z = 6 + 9 - 8 * 2 / 3
+print(z)
+
+z = 6 + 9 - 8 * (2 / 3) 
+print(z)
+
+
+w = (4 + 3 - 3) * 2 / 3
+print(w)
+
+y = 4^3
+
+11 / 5
+
+11 %% 5
+ 11 %/% 5
+ 
